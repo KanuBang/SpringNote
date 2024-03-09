fd2227e83717b80a02849b7d553ea06df9ad8d4c
gdrffg <gdrffg@gmail.com>
2024-03-09 17:59:55 +0900

    FEAT: RateDiscountPolicy
    JAVA
    역할뿐만 아니라 구현에 의존하는 문제가 발생. 구현체를 바꿀 때마다 클라이언트 코드 변경이 필요.
    
    Solution
    클라이언트가 역할에만 의존하도록 코드를 수정하자

--- core/src/main/java/hello/core/Order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/Order/OrderServiceImpl.java
@@ -2,13 +2,15 @@ package hello.core.Order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 
 public class OrderServiceImpl implements OrderService{
     private final MemberService memberService = new MemberServiceImpl();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    // private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberService.findMember(memberId);
---
+++ core/src/test/java/hello/core/discount/RateDiscountPolicyTest.java
@@ -0,0 +1,40 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import javax.swing.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RateDiscountPolicyTest {
+    RateDiscountPolicy rateDiscountPolicy = new RateDiscountPolicy();
+
+    @Test
+    @DisplayName("vip는 10%로 할인해준다.")
+    void vip_o() {
+        //given
+        Member member = new Member(1L, "Alvarez", Grade.VIP);
+        //when
+        int discount = rateDiscountPolicy.discount(member, 20000);
+        //then
+        Assertions.assertThat(discount).isEqualTo((int)(20000*0.1));
+
+    }
+
+    @Test
+    @DisplayName("vip가 아니면 할인 해주지 않는다")
+    void vip_x() {
+        //given
+        Member member = new Member(1L, "ligard", Grade.BASIC);
+
+        //when
+        int discount = rateDiscountPolicy.discount(member, 20000);
+
+        //then
+        Assertions.assertThat(discount).isEqualTo(0);
+    }
+}
