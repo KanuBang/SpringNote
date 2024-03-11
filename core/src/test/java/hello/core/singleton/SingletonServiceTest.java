f3e4f64beb20bdf6eee7bb71cffbf9b9ae75cc56
gdrffg <gdrffg@gmail.com>
2024-03-11 16:46:13 +0900

    Add: SpringContainer SingleTon Test

--- core/src/test/java/hello/core/singleton/SingletonServiceTest.java
+++ core/src/test/java/hello/core/singleton/SingletonServiceTest.java
@@ -1,8 +1,12 @@
 package hello.core.singleton;
 
 
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.*;
@@ -19,5 +23,21 @@ class SingletonServiceTest {
 
         assertThat(singletonService1).isEqualTo(singletonService2);
         singletonService1.logic();
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너는 싱글톤 컨테이너")
+    // 싱글톤 컨테이너에 있는 객체들은 모두 싱글톤 패턴으로 관리됨.
+    // 싱클톤 객체를 생성하고 관리하는 기능을 싱글톤 레지스트리라고 한다.
+    public void springTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        System.out.println("memberService1: " + memberService1);
+        System.out.println("memberService2: " + memberService2);
+
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
