40a226760250039c9c5bddbdcf8767d912c87386
gdrffg <gdrffg@gmail.com>
2024-03-15 14:21:18 +0900

    Add: Automatic Dependency Injection 2

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,17 +4,22 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+// @RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
 
+    /* 필드 주입
     @Autowired private MemberRepository memberRepository;
     @Autowired private DiscountPolicy discountPolicy;
+     */
 
-    // 의존 관계 자동 주입 시, 생서자가 1개 있으면 @Autowired 어노테이션 생략 가능
-
+    /* 의존 관계 자동 주입 시, 생서자가 1개 있으면 @Autowired 어노테이션 생략 가능 */
     @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         System.out.println("생성자 memberRepository: " + memberRepository);
@@ -24,20 +29,22 @@ public class OrderServiceImpl implements OrderService{
     }
 
 
-    // 수정자 주입
 
+    /* 수정자 주입
     @Autowired
     public void setMemberRepository(MemberRepository memberRepository) {
         System.out.println("수정자 memberRepository: " + memberRepository);
         this.memberRepository = memberRepository;
     }
+
     @Autowired
     public void setDiscountPolicy(DiscountPolicy discountPolicy) {
         System.out.println("수정자 discountPolicy: " + discountPolicy);
         this.discountPolicy = discountPolicy;
     }
+    */
 
-    // 일반 메서드 주입
+    /*일반 메서드 주입
     @Autowired
     public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         System.out.println("일반 메서드 주입 memberRepository: " + memberRepository);
@@ -45,6 +52,8 @@ public class OrderServiceImpl implements OrderService{
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
+    */
+
     public MemberRepository getMemberRepository() {
         return memberRepository;
     }
