0cff03f0a244d994db16715b5a1a180764ed0e29
gdrffg <gdrffg@gmail.com>
2024-03-20 13:56:02 +0900

    Add: Bean LifeCycle 2

--- core/src/main/java/hello/core/lifestyle/NetworkClient.java
+++ core/src/main/java/hello/core/lifestyle/NetworkClient.java
@@ -3,7 +3,7 @@ package hello.core.lifestyle;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient{
     private String url;
 
     public NetworkClient() {
@@ -28,17 +28,15 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    // `InitializingBean`은 `afterPropertiesSet()` 메서드로 초기화를 지원한다.
-    @Override
-    public void afterPropertiesSet() throws Exception {
-        System.out.println("초기화 콜백 발동 완료");
+    //
+    public void init() {
+        System.out.println("NetworkClient.init");
         connect();
         call("초기화 연결 메시지");
     }
 
-    // `DisposableBean` 은 `destroy()` 메서드로 소멸을 지원한다.
-    @Override
-    public void destroy() throws Exception {
+    public void close() {
+        System.out.println("NetworkClient.close");
         disConnect();
     }
 
--- core/src/test/java/hello/core/lifestyle/BeanLifeCycleTest.java
+++ core/src/test/java/hello/core/lifestyle/BeanLifeCycleTest.java
@@ -12,18 +12,17 @@ public class BeanLifeCycleTest {
     public void lifeCycleTest( ) {
         ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
         NetworkClient client = ac.getBean(NetworkClient.class);
-        // 소멸전 콜백 -> 종료
         ac.close(); //스프링 컨테이너를 종료, ConfigurableApplicationContext 필요
     }
 
-
     @Configuration
     static class LifeCycleConfig {
-        @Bean
+        //이 메서드 자체를 빈 생성 + 의존관계 주입이라고 봐야 핟나.
+        //이거 이후에 초기화 콜백이 사용되는 거다.
+        @Bean(initMethod = "init", destroyMethod = "close")
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("http://hello-spring.dev");
-            System.out.println("초기화 작업 후 완료 -> 초기화 콜백 발동");
             return networkClient;
         }
     }
