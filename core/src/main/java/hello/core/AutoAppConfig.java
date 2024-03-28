f8578ed0d431a98eb44ec4f8d154f85bae29f188
gdrffg <gdrffg@gmail.com>
2024-03-28 13:59:19 +0900

    Add: Bean Scope 2 (Request Scope 1)

--- core/src/main/java/hello/core/AutoAppConfig.java
+++ core/src/main/java/hello/core/AutoAppConfig.java
@@ -4,6 +4,7 @@ package hello.core;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 @ComponentScan(
