006653d6daffef7ceb43628ad231723b8cd3d5a0
gdrffg <gdrffg@gmail.com>
2024-04-16 12:09:54 +0900

    Add: 역방향 참조(양쪽에 값을 설정하지 않은 상태)

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -6,6 +6,8 @@ import hellojpa.practice.Mate;
 import hellojpa.practice.Team;
 import jakarta.persistence.*;
 
+import java.util.List;
+
 public class JpaMain {
 
     public static void main(String[] args) {
@@ -20,27 +22,38 @@ public class JpaMain {
         try {
 
             Mate mate1 = new Mate();
-            mate1.setName("son");
+            mate1.setName("grimaldo");
             Mate mate2 = new Mate();
-            mate2.setName("romero");
+            mate2.setName("xhaka");
 
             Team team1 = new Team();
-            team1.setName("spurs");
+            team1.setName("bayer04");
 
             mate1.setTeam(team1);
             mate2.setTeam(team1);
-            team1.getMates().add(mate1);
 
+            // 쓰기 지연 저장소에 INSERT 쿼리가 저장됨
             em.persist(mate1);
             em.persist(mate2);
             em.persist(team1);
 
+            // 쓰기 지연 저장소에 있는 쿼리들을 DB에 반영, 동기화
+            em.flush();
+
+            // 영속성 컨텍스트 종료, 1차 캐시 종료
+            em.clear();
+
+            // 1차 캐시에 없으므로 DB에서 조회 -> 1차 캐시에 정보 저장
+            Team findTeam = em.find(Team.class, team1.getId());
+            // 역방향 참조
+            List<Mate> mates = findTeam.getMates();
+
+            for(Mate m : mates) {
+                System.out.println("m = " + m.getName());
+            }
+
             tx.commit();
 
-            //커밋한 후에 봐야 한다.
-            //역방향 참조 (onetomany)
-            Team findTeam = em.find(Team.class, team1.getId());
-            System.out.println(findTeam.getMates());
         } catch (Exception e) {
             tx.rollback();
         } finally {
