56acf173e7c86e0ac92959f0ed7f143dc7add1a9
gdrffg <gdrffg@gmail.com>
2024-05-06 18:25:57 +0900

    Add: MappedSuperclass

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -1,10 +1,12 @@
 package hellojpa;
 
+import hellojpa.domain.Member;
 import hellojpa.practice.Locker;
 import hellojpa.practice.Mate;
 import hellojpa.practice.Student;
 import hellojpa.practice.Team;
 import hellojpa.superMapping.Movie;
+import hellojpa.superMapping.Seller;
 import jakarta.persistence.*;
 
 public class JpaMain {
@@ -19,18 +21,16 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Movie movie = new Movie();
-            movie.setDirector("uuu");
-            movie.setActor("chris");
-            movie.setName("Avengers");
-            movie.setPrice(10000);
-            em.persist(movie);
+            Seller seller = new Seller();
+            seller.setName("Musk");
+            seller.setShopName("tesla");
+            em.persist(seller);
 
             em.flush();
             em.clear();
 
-            Movie findMovie = em.find(Movie.class, movie.getId());
-            System.out.println("Movie: " + findMovie.getName() + ", Director: " + findMovie.getDirector());
+            Seller findSeller = em.find(Seller.class, seller.getId());
+            System.out.println("Seller: " + findSeller.getName() + ", shop name: " + findSeller.getShopName());
             tx.commit();
 
         } catch (Exception e) {
---
+++ ex1-hello-jpa/src/main/java/hellojpa/superMapping/Solder.java
@@ -0,0 +1,17 @@
+package hellojpa.superMapping;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Solder extends BaseEntity{
+    private String email;
+
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
