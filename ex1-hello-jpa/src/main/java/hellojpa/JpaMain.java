9ef1f9b9071d3392f042032a186d54ca11eea1bd
gdrffg <gdrffg@gmail.com>
2024-05-09 21:53:25 +0900

    Add: Embedded Type2

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -18,7 +18,10 @@ import hellojpa.superMapping.Seller;
 import jakarta.persistence.*;
 import org.hibernate.Hibernate;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
 
 public class JpaMain {
 
@@ -32,47 +35,60 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Date date = new Date();
 
-            // 일반 Embedded 연습
-            Period period = new Period();
-            period.setStartDate(date);
-            period.setEndDate(date);
+            //엔티티 생성
+            Guest guest = new Guest();
+            guest.setUsername("chan-wu");
+            guest.setHomeAddress(new Address("homeCity", "street", "10000"));
 
-            Address homeAddress = new Address();
-            homeAddress.setCity("home");
-            homeAddress.setStreet("home park");
-            homeAddress.setZipcode("home17002");
+            //음식 추가
+            guest.getFavoriteFoods().add("치킨");
+            guest.getFavoriteFoods().add("피자");
+            guest.getFavoriteFoods().add("파스타");
+            System.out.println(guest.getFavoriteFoods().toString());
 
-            Member member = new Member();
-            member.setName("marco reus");
-            member.setPeriod(period);
-            member.setAddress(homeAddress);
+            //주소 기록 목록 추가
+            guest.getAddressHistory().add(new Address("old1", "Street1", "20000"));
+            guest.getAddressHistory().add(new Address("old2", "Street2", "30000"));
+            System.out.println(guest.getAddressHistory().toString());
 
-            em.persist(member);
+            // Member 엔티티를 영속성 컨텍스트에 저장 -> Cascade.persist 처럼 작용
+            em.persist(guest); // 값 타입 컬렉션도 Cascade.persist AND Cascade.remove 처럼 작동한다.
 
-            // @AttributeOverride와 @AttributeOverrides
-            Shipment shipment = new Shipment();
+            em.flush();
+            em.clear();
 
-            Address workAddress = new Address();
-            workAddress.setCity("work");
-            workAddress.setStreet("work park");
-            workAddress.setZipcode("work17002");
+            // 이때, 값 타입 컬렉션으로 끌고 오지는 않음.(지연로딩)
+            // 값 타인 컬렉션을 즉시 로딩으로 설정하면 실제로 Guest 조회할 때 한 거 번에 데이터 끌고 오는 쿼리 문을 확인할 수 있음.
 
-            shipment.setHomeAddress(homeAddress);
-            shipment.setWorkAddress(workAddress);
-            em.persist(shipment);
+            Guest findGuest = em.find(Guest.class, guest.getId());
+            //getFavoriteFoods()를 호출하는 시점에 데이터베이스에서 해당 데이터를 로드하는 지연 로딩이 발생합니다.
+            List<String> findFoods = findGuest.getFavoriteFoods();
 
-            //값 비교
-            TestEntity testEntity1 = new TestEntity(1L, "testEntity1");
-            TestEntity testEntity2 = new TestEntity(1L, "testEntity1");
-            System.out.println("testEntity1 == testEntity2: " + (testEntity1 == testEntity2));
-            System.out.println("testEntity1.equals(testEntity2): " + testEntity1.equals(testEntity2));
+            for(String food: findFoods) {
+                System.out.println("food: " + food);
+            }
 
+            List<Address> findAddress = findGuest.getAddressHistory();
+            for(Address address : findAddress) {
+                System.out.printf("city: %s, street: %s, zipcode: %s\n", address.getCity(), address.getStreet(), address.getZipcode());
+            }
 
+            // 값 타입 컬렉션을 수정할 때는 왼전히 새로 -> 값 타입 공유 참조를 피하기 위해
+            findGuest.setHomeAddress(new Address("yong-in","평촌로","17002"));
+
+            // 치킨 -> 한식
+            findGuest.getFavoriteFoods().remove("치킨");
+            findGuest.getFavoriteFoods().add("한식");
+
+            // 새로운 주소
+            findGuest.getAddressHistory().remove(new Address("old1", "Street1", "20000"));
+            findGuest.getAddressHistory().add(new Address("new1", "new_street1", "999"));
             tx.commit();
+
         } catch (Exception e) {
             tx.rollback();
+            e.printStackTrace(); // 예외 정보 출력 추가
         } finally {
             em.close();
         }
---
+++ ex1-hello-jpa/src/main/java/hellojpa/embeddedType/Guest.java
@@ -0,0 +1,75 @@
+package hellojpa.embeddedType;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+public class Guest {
+
+    @Id @GeneratedValue
+    @Column(name = "GUEST_ID")
+    private Long id;
+
+    @Column(name = "USERNAME")
+    private String username;
+
+    @Embedded
+    private Address homeAddress;
+
+    @ElementCollection(fetch = FetchType.LAZY) // 디폴트가 지연로딩, 여기서는 그냥 명시적으로 해보면
+    @CollectionTable(name="FAVORITE_FOOD", joinColumns = @JoinColumn(name = "GUEST_ID"))
+    @Column(name = "FOOD_NAME")
+    private List<String> favoriteFoods = new ArrayList<>();
+
+    @ElementCollection(fetch = FetchType.LAZY)
+    @CollectionTable(name="ADDRESS", joinColumns = @JoinColumn(name = "GUEST_ID"))
+    private List<Address> addressHistory = new ArrayList<>();
+
+
+    public Guest() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Address getHomeAddress() {
+        return homeAddress;
+    }
+
+    public void setHomeAddress(Address homeAddress) {
+        this.homeAddress = homeAddress;
+    }
+
+    public List<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(List<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+    public List<Address> getAddressHistory() {
+        return addressHistory;
+    }
+
+    public void setAddressHistory(ArrayList<Address> addressHistory) {
+        this.addressHistory = addressHistory;
+    }
+}
