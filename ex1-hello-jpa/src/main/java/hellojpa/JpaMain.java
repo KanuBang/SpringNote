d04176c7a18c548d170995f58aedc541c4bd1903
gdrffg <gdrffg@gmail.com>
2024-05-13 18:08:28 +0900

    Add: 페이징 API

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -39,92 +39,24 @@ public class JpaMain {
 
         try {
 
-            Member member1 = new Member();
-            member1.setUsername("rice");
-            member1.setAddress(new Address("london", "north", "17002-1"));
-            em.persist(member1);
-
-            Member member2 = new Member();
-            member2.setUsername("henry");
-            member2.setAddress(new Address("london", "north", "17002-2"));
-            em.persist(member2);
-
-            Member member3 = new Member();
-            member3.setUsername("antony");
-            member3.setAddress(new Address("manchester", "old", "88332-1"));
-            em.persist(member3);
-
-            Team team1 = new Team();
-            team1.setName("ARS");
-            team1.addMember(member1);
-            team1.addMember(member2);
-            em.persist(team1);
-
-            Team team2 = new Team();
-            team2.setName("MU");
-            team2.addMember(member3);
-            em.persist(team2);
-
+            for(int i =0; i < 100; i++) {
+                Member member = new Member();
+                member.setUsername("name " + i);
+                member.setAge(i);
+                em.persist(member);
+            }
 
             em.flush(); //쿼리 DB에 전송
             em.clear(); //엔티티 영속성 해제
 
-            //엔티티 프로젝션 -> 프로젝션 되는 엔티티들 모두 영속성 컨텍스트에 저장된다.
-            TypedQuery<Member> member = em.createQuery("select m from Member m", Member.class);
-            List<Member> memberResultList = member.getResultList();
-            System.out.println("======action1======");
+            TypedQuery<Member> query1 = em.createQuery("select m from Member m order by m.age desc", Member.class)
+                            .setFirstResult(0).setMaxResults(10);
 
-            for(Member m: memberResultList) {
-                //LAZY fetch
-                System.out.println(m.getUsername() +" " + m.getTeam().getName());
+            List<Member> members = query1.getResultList();
+
+            for (Member member: members) {
+                System.out.println(member.getUsername() + " " + member.getAge());
             }
-            System.out.println("======action1======");
-
-            //엔티티 프로젝션
-            TypedQuery<String> username = em.createQuery("select m.username from Member m", String.class);
-            List<String> usernameResultList = username.getResultList();
-            System.out.println("======action2======");
-            for(String n: usernameResultList) {
-                System.out.println("name is " + n);
-            }
-            System.out.println("======action2======");
-
-            //임베디드 타입 프로젝션
-            String myName = "rice";
-            TypedQuery<Address> address = em.createQuery("select m.address from Member m where m.username =: username ", Address.class).setParameter("username", myName);
-            Address addressSingleResult = address.getSingleResult();
-            System.out.println("======action3======");
-            System.out.println(addressSingleResult.getCity() + " " +addressSingleResult.getStreet() +" " + addressSingleResult.getZipcode());
-            System.out.println("======action3======");
-
-
-            // 멤버의 username과 age를 조회
-            TypedQuery<Object[]> query = em.createQuery(
-                    "SELECT m.username, m.age FROM Member m", Object[].class);
-            List<Object[]> resultList = query.getResultList();
-            // 결과 출력
-            System.out.println("======action4======");
-            if (!resultList.isEmpty()) {
-                Object[] result = resultList.get(0);
-                System.out.println("Username = " + result[0]);
-                System.out.println("Age = " + result[1]);
-            }
-            System.out.println("======action4======");
-
-
-            //스칼라 타입 프로젝션, new 명령어로 조회, 순서와 타입이 일치하는 생성자 필요, 단순값을 DTO로 바로 조회
-            String myTeam = "MU";
-            TypedQuery<MemberDTO> result = em.createQuery("select new MemberDTO(m.username, m.age) from Member m where m.team.name = :myTeam", MemberDTO.class)
-                    .setParameter("myTeam", myTeam);
-
-            // 결과 출력
-            MemberDTO memberDTO = result.getResultList().get(0);
-            System.out.println("======action5======");
-            System.out.println("MemberDTO = " + memberDTO.getUsername());
-            System.out.println("MemberDTO = " + memberDTO.getAge());
-            System.out.println("======action5======");
-
-
 
             tx.commit();
 
--- ex1-hello-jpa/src/main/java/hellojpa/jpql/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/jpql/Member.java
@@ -10,20 +10,9 @@ public class Member {
     private String username;
     private int age;
 
-    @Embedded
-    private Address address; // 프로젝션 테스트 용으로 삽입함.
-
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "TEAM_ID")
     private Team team;
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
 
     public Long getId() {
         return id;
