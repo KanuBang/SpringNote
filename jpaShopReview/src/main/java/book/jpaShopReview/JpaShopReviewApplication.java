dd01f24b02c5339ebd629d0a3605c07381472bf2
gdrffg <gdrffg@gmail.com>
2024-06-01 22:28:31 +0900

    Add; project setting

---
+++ jpaShopReview/src/main/java/book/jpaShopReview/JpaShopReviewApplication.java
@@ -0,0 +1,13 @@
+package book.jpaShopReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaShopReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaShopReviewApplication.class, args);
+	}
+
+}
