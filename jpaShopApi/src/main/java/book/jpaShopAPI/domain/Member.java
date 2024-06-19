86d02d26d1be31dd8f84112ff7665faf31b535c1
gdrffg <gdrffg@gmail.com>
2024-06-19 16:45:49 +0900

    Add: 샘플 데이터 입력

--- jpaShopApi/src/main/java/book/jpaShopAPI/domain/Member.java
+++ jpaShopApi/src/main/java/book/jpaShopAPI/domain/Member.java
@@ -16,6 +16,9 @@ public class Member {
     @Column(name = "MEMBER_ID")
     private Long id;
 
+    // 엔티티에 API 검증을 위한 로직이 들어간다. (@NotEmpty 등등)
+    // 실무에서는 회원 엔티티를 위한 API가 다양하게 만들어지는데, 한 엔티티에 각각의 API를 위한
+    // 모든 요청 요구사항을 담기는 어렵다.
     @NotEmpty
     private String name;
 
