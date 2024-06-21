0ebff4253c159d7e3fdb93492808cc7f51034c36
gdrffg <gdrffg@gmail.com>
2024-06-21 17:41:21 +0900

    add: 주문 조회 V1 엔티티 직접 노출

---
+++ jpaShopApi/src/main/java/book/jpaShopAPI/api/OrderApiController.java
@@ -0,0 +1,33 @@
+package book.jpaShopAPI.api;
+
+import book.jpaShopAPI.domain.Order;
+import book.jpaShopAPI.domain.OrderItem;
+import book.jpaShopAPI.domain.OrderSearch;
+import book.jpaShopAPI.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.aspectj.weaver.ast.Or;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName(); // Lazy 강제 초기화
+            order.getDelivery().getAddress(); // Lazy 강제 초기화
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName()); // lazy 강제
+        }
+
+
+        return all;
+    }
+}
