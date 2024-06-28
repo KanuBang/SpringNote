3c3ee4fc10c8c639a3dde19a30abf412e26b8c1f
gdrffg <gdrffg@gmail.com>
2024-06-28 15:04:02 +0900

    settings

---
+++ datajpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,38 @@
+package study.datajpa.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("memberA");
+
+        Member savedMember = memberRepository.save(member);
+
+        // 있을 수도 없을 수도 있으니까 Optional로 제공
+        // Optional<Member> example = memberRepository.findById(savedMember.getId());
+
+        Member findMember = memberRepository.findById(savedMember.getId()).get();
+
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getName()).isEqualTo(member.getName());
+        Assertions.assertThat(findMember).isEqualTo(member);
+
+    }
+}
