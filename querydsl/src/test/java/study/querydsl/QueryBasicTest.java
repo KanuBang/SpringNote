1a79f222cfa6abef335746b9445f13cf300b1589
gdrffg <gdrffg@gmail.com>
2024-07-10 16:23:53 +0900

    add: 여러 가지 조인, 페치 조인

--- querydsl/src/test/java/study/querydsl/QueryBasicTest.java
+++ querydsl/src/test/java/study/querydsl/QueryBasicTest.java
@@ -5,7 +5,9 @@ import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.PersistenceUnit;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -28,6 +30,9 @@ public class QueryBasicTest {
 
     @PersistenceContext
     EntityManager em;
+
+    @PersistenceUnit
+    EntityManagerFactory emf;
 
     JPAQueryFactory queryFactory;
 
@@ -57,9 +62,7 @@ public class QueryBasicTest {
 
     @Test
     public void startQuerydsl() {
-
         QMember m = new QMember("m");
-
         Member findMember = queryFactory.select(m).from(m).where(m.username.eq("member1")).fetchOne();
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
@@ -210,5 +213,76 @@ public class QueryBasicTest {
         assertThat(teamA.get(member.age.avg())).isEqualTo(15);
         assertThat(teamB.get(team.name)).isEqualTo("teamB");
         assertThat(teamB.get(member.age.avg())).isEqualTo(35);
+    }
+
+    @Test
+    public void join() throws Exception {
+        //연관관계가 있는 조인
+        List<Member> result = queryFactory.selectFrom(member)
+                .join(member.team, team)
+                .where(team.name.eq("teamA"))
+                .fetch();
+
+        assertThat(result).extracting("username").containsExactly("member1", "member2");
+    }
+
+    @Test
+    public void theta_join() throws Exception {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+        List<Member> result = queryFactory.selectFrom(member).from(member, team).where(member.username.eq(team.name)).fetch();
+        assertThat(result).extracting("username").containsExactly("teamA","teamB");
+    }
+
+
+    @Test
+    public void join_on_filtering() throws Exception {
+        List<Tuple> result = queryFactory.select(member, team).from(member).leftJoin(member.team, team).on(team.name.eq("teamA")).fetch();
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    @Test
+    public void join_on_no_relation() throws Exception {
+        em.persist(new Member("teamA"));
+        em.persist(new Member("teamB"));
+
+        List<Tuple> result = queryFactory.select(member, team).from(member).leftJoin(team).on(member.username.eq(team.name)).fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("t= " + tuple);
+        }
+
+    }
+
+    @Test
+    public void fetchJoinNo() throws Exception {
+        em.flush();
+        em.clear();
+
+        Member member1 = queryFactory
+                .select(member)
+                .where(member.username.eq("member1"))
+                .from(member)
+                .fetchOne();
+
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(member1.getTeam());
+        assertThat(loaded).as("페치 조인 미적용").isFalse();
+    }
+
+    @Test
+    public void fetchJoinUse() throws Exception {
+        em.flush();
+        em.clear();
+
+        Member findMember = queryFactory.selectFrom(member)
+                .join(member.team, team).fetchJoin()
+                .where(member.username.eq("member1"))
+                .fetchOne();
+
+        boolean loaded = emf.getPersistenceUnitUtil().isLoaded(findMember.getTeam());
+        assertThat(loaded).as("페치 조인 적용").isTrue();
+
     }
 }
