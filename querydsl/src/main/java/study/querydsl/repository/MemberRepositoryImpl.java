04040a3ee192af0f261e5c4ff58123327020956f
gdrffg <gdrffg@gmail.com>
2024-07-12 15:04:04 +0900

    add: 사용자 정의 리포지토리

---
+++ querydsl/src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -0,0 +1,64 @@
+package study.querydsl.repository;
+
+import com.querydsl.core.types.Predicate;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.springframework.util.StringUtils;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.dto.QMemberTeamDto;
+import study.querydsl.entity.QMember;
+import study.querydsl.entity.QTeam;
+
+import java.util.List;
+
+import static study.querydsl.entity.QMember.member;
+import static study.querydsl.entity.QTeam.team;
+
+public class MemberRepositoryImpl implements MemberRepositoryCustom{
+
+    private final JPAQueryFactory queryFactory;
+    /*
+    구체적으로, Spring Data JPA가 MemberRepositoryImpl 클래스의 인스턴스를 생성할 때,
+    EntityManager를 생성자 주입 방식으로 전달합니다.
+    MemberRepositoryImpl 클래스는 MemberRepositoryCustom 인터페이스를 구현하는 커스텀 리포지토리로,
+    기본 리포지토리와 함께 사용됩니다.
+     */
+    public MemberRepositoryImpl(EntityManager em) {
+        this.queryFactory = new JPAQueryFactory(em);
+    }
+
+    @Override
+    public List<MemberTeamDto> search(MemberSearchCondition condition) {
+        return queryFactory
+                .select(new QMemberTeamDto(
+                        member.id,
+                        member.username,
+                        member.age,
+                        team.id,
+                        team.name))
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(usernameEq(condition.getUsername()),
+                        teamnameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+                .fetch();
+    }
+
+    private BooleanExpression usernameEq(String username) {
+        return StringUtils.isEmpty(username) ? null : member.username.eq(username);
+    }
+    private BooleanExpression teamnameEq(String teamName) {
+        return StringUtils.isEmpty(teamName) ? null : member.team.name.eq(teamName);
+    }
+    private BooleanExpression ageGoe(Integer ageGoe) {
+        return ageGoe == null ? null : member.age.goe(ageGoe);
+    }
+    private BooleanExpression ageLoe(Integer ageLoe) {
+        return ageLoe == null? null : member.age.loe(ageLoe);
+    }
+
+}
